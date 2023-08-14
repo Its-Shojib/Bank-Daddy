920177e54d7119214b3b2a1c617561c22aad5faa
Its_Shojib <mdshojib922@gmail.com>
2023-08-14 19:53:45 +0600

    Login Completed

---
+++ Js/login.js
@@ -0,0 +1,15 @@
+// Step-1
+document.getElementById("btn-submit").addEventListener("click",function(){
+    let emailField = document.getElementById("email");
+    let email = emailField.value;
+    let passwordField = document.getElementById("password");
+    let password = passwordField.value;
+    if(email === 'mdshojib922@gmail.com' && password === '190103'){
+        window.location.href="bank.html";
+    }
+    else{
+        document.getElementById("fall").innerText = "Please Enter Valid Email and Password";
+
+        // console.log("Please Enter Valid Email and Password");
+    }
+});
